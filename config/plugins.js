2706b647cc2b8451efdce0ad9740840675876631
dhirajOpeninfo <dhirajs.openinfotech@gmail.com>
2024-03-02 23:53:45 +0530

    documentation

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+   // Other plugin configurations...
+   documentation: {
+     config: {
+       swaggerOptions: {
+         // Your custom Swagger UI options
+       },
+       // Other documentation plugin options
+     },
+   },
+   // Other configurations...
+ };
